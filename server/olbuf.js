63db7e2d1579a148137e07d5d0e9a695d4a327a3
Liam Doherty <lfdoherty@gmail.com>
2012-08-21 23:57:17 -0700

    performance optimizations (especially the simple-subset-of-type case), bugfixes, scalability improvement (temporary ol serialization of state), parsicle and baleen removed as dependencies

---
+++ server/olbuf.js
@@ -0,0 +1,210 @@
+"use strict";
+
+var fparse = require('fparse')
+var _ = require('underscorem')
+
+var bufw = require('./../util/bufw')
+var bin = require('./../util/bin')
+
+var shared = require('./tcp_shared')
+var fp = shared.editFp
+
+function serializeFrame(edits, w){
+	w.putInt(edits.length)
+	//console.log('putting int: ' + edits.length)
+	//edits.forEach(function(e){
+	for(var i=0;i<edits.length;++i){
+		var e = edits[i]
+		w.putByte(fp.codes[e.op])
+		w.putInt(e.editId)
+		fp.writers[e.op](w, e.edit)
+	}
+}
+
+var rs = fparse.makeRs()
+var r = rs.s
+var readersByCode = fp.readersByCode
+var skippersByCode = fp.skippersByCode
+function deserializeFrame(frame){
+	rs.put(frame)
+	var many = r.readInt()
+	var edits = []
+
+	for(var i=0;i<many;++i){
+		var b = r.readByte()
+		_.assert(b > 0)
+		var editId = r.readInt()
+		var e = readersByCode[b](r)
+		edits.push({op: fp.names[b], edit: e, editId: editId})
+	}
+	rs.assertEmpty()
+	return edits
+}
+
+function deserializePartOfFrame(frame, startEditId, endEditId){
+	rs.put(frame)
+	var many = r.readInt()
+	var edits = []
+
+	for(var i=0;i<many;++i){
+		var b = r.readByte()
+		_.assert(b > 0)
+		var editId = r.readInt()
+		if(editId >= startEditId && editId < endEditId){
+			var e = readersByCode[b](r)
+			edits.push({op: fp.names[b], edit: e, editId: editId})
+		}else{
+			skippersByCode[b](r)
+		}
+	}
+	rs.assertEmpty()
+	return edits
+}
+
+function appendSerializeFrame(rest, edits, w){
+	_.assert(rest.length > 4)
+	var manyBefore = bin.readInt(rest, 0)
+	w.putInt(manyBefore + edits.length)
+	w.putData(rest, 4, rest.length)//.slice(4))
+	//var str = ''
+	//for(var i=4;i<rest.length;++i){
+	//	str += rest[i]+' '
+	//}
+	//console.log('@'+str)
+	//console.log('& ' + JSON.stringify(deserializeFrame(rest)))
+	//console.log('^ ' + JSON.stringify(edits))
+	//edits.forEach(function(e){
+	for(var i=0;i<edits.length;++i){
+		var e = edits[i]
+		w.putByte(fp.codes[e.op])
+		w.putInt(e.editId)
+		fp.writers[e.op](w, e.edit)
+	}
+	
+	
+}
+
+exports.make = function(){
+
+	var index = {}
+	var buffers = []
+	var bufferStarts = []//relatively fast way to work out what buffer a total offset is in
+
+	var currentStart = 0	
+	var ws = {
+		write: function(buf){
+			buffers.push({
+				buf: buf,
+				start: currentStart
+			})
+			bufferStarts.push(currentStart)
+			currentStart += buf.length
+		},
+		end: function(){
+		}
+	}
+	var w = new bufw.W(10*1024*1024, ws)
+	
+	
+	var handle = {
+		write: function(id, edits){
+			_.assertInt(id)
+			_.assertArray(edits)
+			
+			var offset = w.getCurrentOffset()
+			
+			if(edits.length >= 2) _.assertEqual(edits[1].edit.id, id)
+			//write all edits
+			w.startLength()//set up the length frame
+			serializeFrame(edits, w)
+			w.endLength()
+			
+			index[id] = offset
+			
+			//if(Math.random() < .01) console.log('olbuf: ' + w.getCurrentOffset())
+		},
+		append: function(id, edits){
+			_.assertInt(id)
+			_.assertArray(edits)
+
+			var offset = currentStart + w.getCurrentOffset()
+			
+			//console.log('appending(' + offset + '): ' + JSON.stringify(edits))
+			
+			var rest = handle.getBinary(id)
+			w.startLength()//set up the length frame
+			appendSerializeFrame(rest, edits, w)
+			w.endLength()
+			
+			var b =  w.getBackingBuffer()
+			/*var str = ''
+			for(var i=0;i<w.getCurrentOffset();++i){
+				str += b[i]+' '
+			}
+			console.log('$'+str)*/
+			
+			var old = index[id]
+			index[id] = offset
+			_.assert(offset !== old)
+			_.assert(rest.length < handle.getBinary(id).length)
+			
+			_.assert(handle.get(id).length >= 2)
+
+			//if(Math.random() < .01) console.log('olbuf: ' + w.getCurrentOffset())
+
+		},
+		destroy: function(id){
+			index[id] = -1
+		},
+		get: function(id){
+			var res = deserializeFrame(handle.getBinary(id))
+			if(res.length >= 2) _.assertEqual(res[1].edit.id, id)
+			return res
+		},
+		getBinary: function(id){
+			var offset = index[id]
+			_.assert(offset !== -1)
+			//console.log('getting ' + id + ' ' + offset)
+			_.assertInt(offset)
+			var off = 0
+			var b
+			var bOff
+			for(var i=0;i<bufferStarts.length;++i){
+				if(bufferStarts[i] > off){
+					b = buffers[i-1].buf
+					bOff = offset - b.start
+				}
+			}
+			if(!b){
+				b = w.getBackingBuffer()
+				bOff = offset - currentStart
+			}
+			var len = bin.readInt(b, bOff)
+			var frame = b.slice(bOff+4, bOff+4+len)
+			return frame
+		},
+		serializeBinaryRange: function(id, startEditId, endEditId, w){//TODO optimize
+			var frame = handle.getBinary(id)
+			//_.errout('TODO: ' + startEditId + ' ' + endEditId)
+			//deserializePartOfFrame(frame, startEditId, endEditId) 
+			
+			var edits = deserializeFrame(frame)
+			var actual = []
+			for(var i=0;i<edits.length;++i){
+				var e = edits[i]
+				_.assertInt(e.editId)
+				if(startEditId <= e.editId && (endEditId === -1 || e.editId <= endEditId)){
+					actual.push(e)
+				}
+			}
+			serializeFrame(actual, w)
+		},
+		isNew: function(id){
+			return index[id] === undefined
+		},
+		serializeEdits: function(edits, w){
+			serializeFrame(edits, w)
+		}
+	}
+	return handle;
+}
---
+++ server/olcache.js
@@ -0,0 +1,204 @@
+"use strict";
+
+var _ = require('underscorem')
+
+var olbuf = require('./olbuf')
+
+var CACHE_SIZE = 10//MUST BE AT LEAST 2?
+
+/*
+olcache implements a cache of the CACHE_SIZE most recent objects, so as to avoid serializing objects undergoing ongoing editing repeatedly.
+
+It uses a ring buffer of the ids, in order of most recently used.  This results in O(1) performance in the repeated case (when
+the same id is used multiple times sequentially) and in the single-use case (when each id is used some number of times repeatedly,
+and then never again.)
+
+*/
+
+exports.make = function(){
+
+	var ob = olbuf.make()
+	
+	var cache = {}
+	var order = []
+	var first = 0
+
+	var many = 0
+	var cacheHasFilled = false
+	
+	function evict(newId){
+		//var oldestIndex = first
+		//if(oldestIndex === -1) oldestIndex = CACHE_SIZE-1
+		
+		var evictedId = order[first]
+		
+		if(evictedId !== -1){//means it has been destroyed
+			// write new version of object
+			var edits = cache[evictedId]
+			if(edits === undefined) _.errout('cannot evict, bug: ' + evictedId + ' ' + JSON.stringify(order))
+			_.assertArray(edits)
+			/*if(edits[1].edit.id !== evictedId){
+				_.errout(JSON.stringify(edits))
+			}
+			_.assertEqual(edits[1].edit.id, evictedId)*/
+		
+			if(ob.isNew(evictedId)){
+				ob.write(evictedId, edits)
+			}else{
+				ob.append(evictedId, edits)
+			}
+			delete cache[evictedId]
+			//console.log('evicted: ' + evictedId)
+		}
+		
+		push(newId)
+		
+	//	console.log('should be full: ' + JSON.stringify(order))
+		/*for(var i=0;i<order.length;++i){
+			_.assertInt(order[i])
+			_.assert(order[i] !== evictedId)
+		}*/
+	}
+	function push(id){
+		order[first] = id
+		//console.log('pushed: ' + id)
+		++first
+		if(first === CACHE_SIZE) first = 0
+	}
+	
+	function rot(f){return f === -1 ? CACHE_SIZE-1 : f;}
+	
+	function incr(id){
+		if(order[rot(first-1)] === id){
+			//console.log('repeated: ' + id)
+			return
+		}
+		//console.log('incr diff(' + id + ')[' + first + ' ' + rot(first-1) + ']: ' + JSON.stringify(order))
+		//shift everything over one, overwriting the old position of the id, and moving the current 'first' id to second place
+		var prev = order.indexOf(id)
+		var distance = first > prev ? first-prev : first + (CACHE_SIZE-prev)
+		for(var d=0;d<distance;++d){
+			var off = d + prev + 1
+			if(off >= CACHE_SIZE) off -= CACHE_SIZE
+			var pOff = off - 1
+			if(pOff === -1) pOff = CACHE_SIZE - 1
+			
+			order[pOff] = order[off]
+		}
+		//console.log('preshift(' + id + ', old: ' + order[first] + '): ' + JSON.stringify(order))
+		order[first] = id
+		//console.log('shifted: ' + id)
+	}
+	var handle = {
+		addEdit: function(id, edit){
+		
+			if(CACHE_SIZE === 0){
+			
+				if(ob.isNew(id)){
+					ob.write(id, [edit])
+				}else{
+					ob.append(id, [edit])
+				}
+				return
+			}
+			
+			//console.log('adding edit: ' + id + ' ')
+		
+			//console.log('id: ' + id)
+			var list = cache[id]
+			
+			if(!ob.isNew(id) && ob.get(id).length >= 2){
+				_.assert(edit.op !== 'made')
+			}
+			
+			if(list === undefined){
+				list = cache[id] = []
+				if(cacheHasFilled){
+					//console.log('evicting')
+					evict(id)
+				}else{
+					order[many] = id
+					//console.log('filling(' + many + '): ' + JSON.stringify(order))
+					++first
+					++many
+					if(many === CACHE_SIZE){
+						cacheHasFilled = true
+						first = 0
+					}
+				}
+			}else{
+				//console.log('incr')
+				//if(list.length >= 2) _.assert(edit.op !== 'made')
+				incr(id)
+			}
+			list.push(edit)
+		},
+		get: function(id){
+			if(cache[id]){
+				if(ob.isNew(id)){
+					return [].concat(cache[id])
+				}else{
+					var res = ob.get(id).concat(cache[id])
+					//console.log("returning combination: " + JSON.stringify(res))
+					//_.errout('TODO?')
+					return res
+				}
+			}else{
+				return ob.get(id)
+			}
+		},
+		serializeBinaryRange: function(id, startEditId, endEditId, w){
+			if(cache[id]){
+				if(ob.isNew(id)){
+					var edits = cache[id]
+					var actual = []
+					for(var i=0;i<edits.length;++i){
+						var e = edits[i]
+						_.assertInt(e.editId)
+						if(startEditId <= e.editId && (endEditId === -1 || e.editId <= endEditId)){
+							actual.push(e)
+						}
+					}
+					if(startEditId === -1) _.assert(actual.length >= 2)
+					//console.log('serializing: ' + JSON.stringify(actual))
+					ob.serializeEdits(actual, w)
+				}else{
+					var edits = ob.get(id).concat(cache[id])
+					var actual = []
+					for(var i=0;i<edits.length;++i){
+						var e = edits[i]
+						_.assertInt(e.editId)
+						if(startEditId <= e.editId && (endEditId === -1 || e.editId <= endEditId)){
+							actual.push(e)
+						}
+					}
+					ob.serializeEdits(actual, w)
+					/*var res = ob.get(id).concat(cache[id])
+					console.log("returning combination: " + JSON.stringify(res))
+					_.errout('TODO?')
+					return res*/
+					//_.errout('TODO?')
+					//ob.serializeBinaryRange(id, startEditId, endEditId, w)
+				}
+			}else{
+				//ob.get(id)
+				//_.errout('TODO')
+				ob.serializeBinaryRange(id, startEditId, endEditId, w)				
+			}
+		},
+		destroy: function(id){
+			if(cache[id]){
+				order[order.indexOf(id)] = -1
+				delete cache[id]
+			}
+			if(!ob.isNew(id)){
+				ob.destroy(id)
+			}
+		},
+		assertUnknown: function(id){
+			_.assert(ob.isNew(id))
+		}
+	}
+	
+	return handle
+}
